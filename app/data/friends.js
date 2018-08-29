29751678df0b0102900f94e1f10cac6572604624
Johnny Neagoe <johnnyneagoe@gmail.com>
2018-08-28 23:54:53 -0400

    03

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,9 +1,30 @@
-var friendsArray = [
+var userData = [
     {
-      friendName: "Ahmed",
-      friendEmail: "ahmed@example.com",
-      friendID: "afhaque89",
-      friendNumber: "000-000-0000"
-    }
+      name: "John",
+      email: "john@gmail.com",
+      scores: ["5", "4", "5", "3", "2", "1", "1", "3", "5", "4"]
+    },
+    {
+      name: "Joe",
+      email: "joe@gmail.com",
+      scores: ["2", "1", "1", "4", "5", "2", "1", "2", "3", "3"]
+    },
+    {
+      name: "Amanda",
+      email: "amanda@gmail.com",
+      scores: ["4", "5", "2", "1", "5", "4", "2", "2", "1", "5"]
+    },
+    {
+      name: "Rachel",
+      email: "Rachel@gmail.com",
+      scores: ["1", "2", "1", "3", "4", "5", "5", "3", "5", "2"]
+    },
+    {
+      name: "Adam",
+      email: "adam@gmail.com",
+      scores: ["4", "4", "5", "2", "2", "1", "3", "2", "3", "1"]
+    },
+
   ];
-  
+
+  module.exports = userData;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,4 +1,5 @@
-var friendsData = require("../data/friends");
+var path = require("path");
+var friendsData = require("../data/friends.js");
 
 module.exports = function(app) {
 
@@ -6,8 +7,28 @@ module.exports = function(app) {
     res.json(friendsData);
   });
 
-  app.post("/api/survey", function(req, res) {
-      friendsData.push(req.body);
-      res.json(req.body);
-  });
+
+app.post("/api/friends", function(req, res) {
+  const userInput = req.body;
+  var userScores = userInput.scores;
+  var matchName = '';
+  var matchEmail = '';
+  var totalDifference = 1000000;
+
+  for (var i = 0; i < friendsData.length; i++){
+    var diff = 0;
+    for (var j = 0; j < userScores.length; j++){
+      diff += Math.abs(friendsData[i].scores[j] - userScores[j]);
+    }
+    if (diff < totalDifference) {
+      totalDifference = diff;
+      matchName = friendsData[i].name;
+      matchEmail = friendsData[i].email;
+    }
+  }
+
+  friendsData.push(userInput);
+  res.json({status: 'OK', matchName: matchName, matchEmail: matchEmail});
+
+});
 }
