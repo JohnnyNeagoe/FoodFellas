128ea13d37d57947a03c917e8fd5629040f190cb
Johnny Neagoe <johnnyneagoe@gmail.com>
2018-08-28 22:02:47 -0400

    02

---
+++ app/data/friends.js
@@ -0,0 +1,9 @@
+var friendsArray = [
+    {
+      friendName: "Ahmed",
+      friendEmail: "ahmed@example.com",
+      friendID: "afhaque89",
+      friendNumber: "000-000-0000"
+    }
+  ];
+  
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,13 @@
+var friendsData = require("../data/friends");
+
+module.exports = function(app) {
+
+  app.get("/api/friends", function(req, res) {
+    res.json(friendsData);
+  });
+
+  app.post("/api/survey", function(req, res) {
+      friendsData.push(req.body);
+      res.json(req.body);
+  });
+}
